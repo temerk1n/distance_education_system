cc00281fa68c4386927e10e29deea7a990a722d0
temerk1n <nikita.verigin23@gmail.com>
2024-03-17 19:47:41 +0300

    feat: add models

--- assessment/models.py
+++ assessment/models.py
@@ -1,3 +1,21 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    submitted_works_count = models.IntegerField(default=0)
+
+
+class PracticalWork(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    submitting_date = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200)
+    file = models.FileField(null=True, upload_to="Uploaded Files/")
+    mark_date = models.DateTimeField(auto_now_add=False, null=True)
+    mark = models.IntegerField(blank=False, null=True)
+
