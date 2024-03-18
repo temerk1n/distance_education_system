cca64faf81b37733b743cc77f90c314178d21cdd
temerk1n <nikita.verigin23@gmail.com>
2024-03-18 19:38:20 +0300

    feat: added students_service

--- assessment/urls.py
+++ assessment/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from rest_framework import routers
 
 from . import views
 
@@ -14,6 +13,16 @@ router.register(r'works', views.WorksViewSet)
 urlpatterns = []
 
 urlpatterns += router.urls
+urlpatterns = [
+    path('students',
+         views.StudentsViewSet.as_view(
+             {'post': 'create', 'get': 'list'}
+         ), name='students'),
+    path('students/<str:id>',
+         views.StudentsViewSet.as_view(
+             {'get': 'retrieve', 'delete': 'destroy'}
+         ), name='student-detail'),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
