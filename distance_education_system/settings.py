7aaf11118d58ad1f5f3237bdceabb8cc4cfacea2
temerk1n <nikita.verigin23@gmail.com>
2024-03-19 19:32:37 +0300

    feat: added component tests for WorksViewSet

--- distance_education_system/settings.py
+++ distance_education_system/settings.py
@@ -47,6 +47,7 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.MultiPartRenderer'
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_PARSER_CLASSES': [
